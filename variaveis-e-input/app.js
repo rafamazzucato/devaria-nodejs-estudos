affd0366cc9e28aa771054fba5b49dea2fc2afc7
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-02-02 21:36:41 -0300

    adicionei mais uma variavel no console interpolado

--- variaveis-e-input/app.js
+++ variaveis-e-input/app.js
@@ -7,7 +7,8 @@ const minhaPrimeiraConstanteString = 'Minha primeira constante';
 console.log(minhaPrimeiraConstanteString);
 
 let leituraDeCampo;
+let teste = 'teste'
 readLine.question('Informe sua idade:', input => {
     leituraDeCampo = input;
-    console.log(`o usuario digitou: ${leituraDeCampo}`);
+    console.log(`o usuario digitou: ${leituraDeCampo} e nossa variavel de teste e: ${teste}`);
 });
